0982641bbe51f8a6aac661e484c387a67a0717b0
이우빈 <125638564+dnqls9875@users.noreply.github.com>
2025-01-23 11:48:36 +0900

    [실습] fom 컨트롤 실습

--- src/components/form-input.tsx
+++ src/components/form-input.tsx
@@ -1,48 +1,16 @@
-import { useId } from 'react';
+import { useId, type ComponentProps } from 'react';
 
-interface FormInputProps {
-  type?:
-    | 'text'
-    | 'range'
-    | 'password'
-    | 'number'
-    | 'search'
-    | 'color'
-    | 'email';
+type FormInputProps = ComponentProps<'input'> & {
   label: string;
-  placeholder?: string;
-  value?: string | number;
-  defaultValue?: string | number;
-  readOnly?: boolean;
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  [property: string | number]: unknown;
-}
+};
 
-function FormInput({
-  type = 'text',
-  label,
-  placeholder,
-  value,
-  defaultValue,
-  readOnly,
-  onChange,
-  ...restProps
-}: FormInputProps) {
+function FormInput({ label, ...restProps }: FormInputProps) {
   const inputId = useId();
 
   return (
     <div className="formControl">
       <label htmlFor={inputId}>{label}</label>
-      <input
-        type={type}
-        id={inputId}
-        placeholder={placeholder}
-        readOnly={readOnly}
-        defaultValue={defaultValue}
-        value={value}
-        onChange={onChange}
-        {...restProps}
-      />
+      <input id={inputId} {...restProps} />
     </div>
   );
 }
--- src/playgrounds/02-react-form.tsx
+++ src/playgrounds/02-react-form.tsx
@@ -1,29 +1,157 @@
+import { useState } from 'react';
 import FormInput from '@/components/form-input';
 
+const formStyles = {
+  display: 'flex',
+  flexFlow: 'column',
+  alignItems: 'start',
+  gap: 20,
+};
+
 function ReactForm() {
+  const [age, setAge] = useState<number>(22);
+  const [color, setColor] = useState<string>('#2483DB');
+  const [limitAge, setLimitAge] = useState<number>(40);
+  const [profileImage, setProfileImage] = useState<string | null>(null);
+
   return (
     <div className="ReactForm">
       <h2>React 폼(form)</h2>
-      <form>
-        <FormInput type="text" label="이름" placeholder="박수무당" />
+      <form style={formStyles}>
+        {/* type=text */}
+        <FormInput label="이름" placeholder="박수무당" />
+
+        {/* type=password */}
         <FormInput
           type="password"
           label="비밀번호"
           placeholder="영어, 숫자 조합 4자리 이상"
         />
+
+        {/* type=email */}
+        <FormInput type="email" label="이메일" placeholder="user@company.io" />
+
+        {/* type=number, controlled component */}
         <FormInput
           type="number"
           label="나이"
-          defaultValue={20}
-          data-name="폼 인풋"
-          data-type="input"
-          title="컴포넌트 만세!"
-          v-for="data in list"
+          value={age}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            const { value } = e.target;
+            // const nextAgeValue: number = +value;
+            // const nextAgeValue: number = Number(value);
+            const nextAgeValue: number = parseInt(value, 10);
+            setAge(nextAgeValue);
+          }}
         />
-        <FormInput type="color" label="색상" />
-        <FormInput type="range" label="시청 허용 나이" />
+
+        {/* type=color */}
+        <FormInput
+          type="color"
+          label="색상"
+          value={color}
+          onChange={(e) => {
+            const { value } = e.target;
+            setColor(value);
+          }}
+        />
+
+        {/* type=range */}
+        <div style={{ display: 'flex', gap: 8, alignItems: 'center' }}>
+          <FormInput
+            type="range"
+            label="시청 허용 나이"
+            min={10}
+            max={90}
+            step={10}
+            value={limitAge}
+            onChange={(e) => {
+              const { value } = e.target;
+              setLimitAge(parseInt(value, 10));
+            }}
+          />
+          <output>{limitAge}</output>
+        </div>
+
+        {/* type=file */}
+        <div style={{ padding: 12, border: '0.5px solid rgba(0 0 0 / 30%)' }}>
+          <FormInput
+            label="프로필"
+            type="file"
+            accept="image/*"
+            onChange={(e) => {
+              const { files } = e.target;
+
+              if (files && files.length > 0) {
+                const [file] = files;
+                const profileImagePath = URL.createObjectURL(file);
+                setProfileImage(profileImagePath);
+              }
+            }}
+          />
+          {/* 업로드 할 이미지 표시 */}
+          {profileImage && (
+            <img
+              style={{ marginBlockStart: 8 }}
+              src={profileImage}
+              alt="업로드 할 프로필"
+              width={100}
+              height={100}
+            />
+          )}
+        </div>
+
+        {/* type=radio */}
+        <fieldset>
+          <legend>성별</legend>
+          <FormInput
+            type="radio"
+            label="남성"
+            name="usergender"
+            value="male"
+            defaultChecked
+          />
+          <FormInput
+            type="radio"
+            label="여성"
+            name="usergender"
+            value="female"
+          />
+        </fieldset>
+
+        {/* type=checkbox */}
+        <fieldset>
+          <legend>취미</legend>
+          <FormInput
+            type="checkbox"
+            label="공부"
+            name="userhobby"
+            value="study"
+            defaultChecked
+          />
+          <FormInput
+            type="checkbox"
+            label="운동"
+            name="userhobby"
+            value="helth"
+          />
+          <FormInput
+            type="checkbox"
+            label="영화 감상"
+            name="userhobby"
+            value="watch-a-movie"
+          />
+        </fieldset>
+
+        {/* type=date */}
+        <FormInput type="date" label="여행 날짜" />
+
+        {/* type=datetime-local */}
+        <FormInput type="datetime-local" label="비행기 출국 시간" />
 
         <button type="submit">제출</button>
+        <button type="reset">초기화</button>
+        {/* <input type="reset" value="초기화" /> */}
       </form>
     </div>
   );

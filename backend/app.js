ea516a74663c85e2747d532d29345846af904e97
이우빈 <125638564+dnqls9875@users.noreply.github.com>
2025-01-24 16:07:37 +0900

    [실습] 마무리

--- backend/app.js
+++ backend/app.js
@@ -1,77 +1,93 @@
+import cors from 'cors';
 import express from 'express';
+import { resolve } from 'node:path';
+import fileUpload from 'express-fileupload';
 import { createUser, isRegisteredUser } from './lib/user.js';
-import { request } from 'node:http';
 
-// Express {}
 const app = express();
-
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(resolve('./public')));
+app.use(fileUpload());
+app.use(cors());
 
 app.post('/api/signin', async (request, response) => {
   const { useremail, userpassword } = request.body;
-  console.log(useremail, userpassword);
 
   if (!useremail || !userpassword) {
     return response
       .status(400)
-      .send('로그인을 시도하려면 이베일, 패스워드 입력이 필요합니다.');
+      .send('로그인을 시도하려면 이메일, 패스워드 입력이 필요합니다.');
   }
 
   const result = await isRegisteredUser(useremail, userpassword); // null or true or false
 
-  // null인 경우, 가입한 적이 없는 사용자 싶패!
+  console.log({ result });
+
+  // null인 경우, 가입한 적이 없는 사용자 실패!
   if (result === null) {
     return response.status(400).send(`
       <p>${useremail} 이메일 계정으로 가입된 적이 없습니다.</p>
-      `);
+    `);
   }
+
   if (result) {
-    // true인 경우, 패스워드가 유요한 사용자 (인증) 성공!
+    // true인 경우, 패스워드가 유효한 사용자 (인증) 성공!
     return response.status(200).send(`
       <p>${useremail} 계정으로 로그인 되었습니다.</p>
-      `);
+    `);
   } else {
     // false인 경우, 패스워드가 유효하지 않은 사용자 실패!
     return response.status(400).send(`
       <p>${useremail} 계정 패스워드가 잘못되었습니다.</p>
-      `);
+    `);
   }
 });
 
 app.post('/api/signup', async (request, response) => {
-  // 회원가입 요청된 사용자 입력 정보
   const { username, useremail, userpassword } = request.body;
 
   if (!username || !useremail || !userpassword) {
     return response.status(400).send(`
-      <p>회원 가입 정보인 "이름", "이메일", "패스워드"를 모두 입력해야 합니다.</p>  
+      <p style="color: red">회원가입에 필요한 이름, 이메일, 패스워드 모두 입력이 필요합니다.</p>
     `);
   }
 
-  // 새 사용자 생성 (백엔드 스토리지)
+  // 파일(files) 정보 접근
+  const profileImage = request.files?.userprofile;
+  let profileImagePath = '';
+
+  if (profileImage) {
+    await profileImage.mv(resolve('public/files', profileImage.name));
+    profileImagePath = `/files/${profileImage.name}`;
+  } else {
+    console.log('이미지 없음');
+  }
 
   try {
+    // 새 사용자 생성 (백엔드 스토리지)
     const newUser = await createUser({
       name: username,
       email: useremail,
       password: userpassword,
+      profileImage: profileImagePath,
     });
 
     if (newUser) {
-      response.status(201).send(`${username}님! 회원가입에 성공했습니다. 😁`);
+      // response
+      //   .status(201)
+      //   .send(`${newUser.name}님! 회원가입에 성공했습니다. 😊`);
+      const { password, ...user } = newUser;
+      response.status(201).json(user);
     } else {
-      response.status(400).send(`${username}님은 회원가입을 이미 하셨습니다.`);
+      response
+        .status(400)
+        .send(`${username}님은 회원 가입을 이미 하셨습니다. 😥`);
     }
   } catch (error) {
-    response.status(500).send('새 사용자 생성에 문제가 발생했습니다.');
+    response.status(500).send('회원가입에 문제가 발생했습니다.');
   }
-
-  console.log(username, useremail, userpassword);
-
-  // ...
 });
 
-// 라우팅(Routing)
 app.get('/api/hello', (request, response) => {
   const { username, useremail } = request.query;
   if (username && useremail) {
